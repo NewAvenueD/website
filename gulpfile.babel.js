4de1a519a0b5d8097fdfa09a69196db6dd4c45df
Adam Romines <adam.romines@gmail.com>
2016-04-07 15:17:53 -0600

    Rewrites gulpfile to fix attepted relaunching of php server.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,36 +3,39 @@ import gulp from 'gulp';
 import sass from 'gulp-sass';
 import sourcemaps from 'gulp-sourcemaps';
 import prefix from 'gulp-autoprefixer';
-import browserSync from 'browser-sync'
+import bSync from 'browser-sync'
 import php from 'gulp-connect-php'
 
-const reload = browserSync.reload;
+let browserSync = bSync.create();
 const scss = './themes/newave/src/scss/**/*.scss';
 const pages = './pages/**/*.md'
 
+
+gulp.task('serve', ['sass'], () => {
+  browserSync.init({
+    proxy:'127.0.0.1:8010',
+    port: 8080,
+    open: true,
+    notify: false
+  });
+
+  gulp.watch(scss, ['sass']);
+  gulp.watch(pages).on('change', browserSync.reload)
+})
 // Server
 gulp.task('php', function() {
     php.server({ base: '../.', port: 8010, keepalive: true});
 });
 
 
-gulp.task('browser-sync', ['php'], () => {
-  browserSync({
-    proxy:'127.0.0.1:8010',
-    port: 8080,
-    open: true,
-    notify: false
-  })
-})
+
 
 gulp.task('sass', () => {
   gulp.src(scss)
     .pipe(sass().on('error', sass.logError))
     .pipe(prefix())
-    .pipe(gulp.dest('./themes/newave/public'));
+    .pipe(gulp.dest('./themes/newave/public'))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('default', ['browser-sync','sass'], () => {
-  gulp.watch(scss, ['sass']);
-  gulp.watch(pages, [reload]);
-});
+gulp.task('default', ['serve']);
--- pages/02.how-it-works/default.md
+++ pages/02.how-it-works/default.md
@@ -3,4 +3,6 @@ title: How it Works
 ---
 Tired of waiting rooms? We are too. That’s why we don’t have one. We know going to the dentist can be a chore, that’s why we’re making it easier for your team by bringing the dental office to you. It’s dentistry, delivered to your workplace.
 
-TODO: Icons?
+
+[comment]: <> (TODO: Icons?)
+[comment]: <> (![](myimage.jpg?classes=float-left))

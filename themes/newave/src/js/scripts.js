fd0bea9c5c22d1bf07ef55a264516fdc5910c670
Adam Romines <adam.romines@gmail.com>
2016-06-16 15:46:28 -0600

    More modular, better styled nav

--- themes/newave/src/js/scripts.js
+++ themes/newave/src/js/scripts.js
@@ -1,7 +1,9 @@
 window.addEventListener('load', function () {
-  console.log('window load event');
+  // console.log('window load event');
   const navigation = document.getElementById('navigation');
-
+  const body = document.querySelector('body');
+  //
+  // toggle (existing) 'data-state' attributes
   const toggleState = function (el, datum, one, two) {
     let elem = document.querySelector(el);
     if ( elem ) {
@@ -13,11 +15,21 @@ window.addEventListener('load', function () {
   }
 
   function toggleNav() {
-    navigation.classList.toggle('open')
-  }
-  // document.getElementById('js-menu-button').onclick = toggleNav;
-  document.getElementById('js-menu-button').onclick = (e) => {
+    let startingState = body.getAttribute('data-state-menu');
+    console.log('startingState: ', startingState);
     toggleState('body', 'menu', 'closed', 'open');
+  }
+  // toggle nav on menu button click
+  document.getElementById('js-menu-button').onclick = (e) => {
+    toggleNav();
     e.preventDefault();
   };
+
+  // toggle nav on body clicks if it is open && click is to outside 'drawer'
+  body.onclick = (e) => {
+    let inSideNav = navigation.contains(e.target);
+    if (body.getAttribute('data-state-menu') === 'open' && !inSideNav) {
+      toggleNav();
+  }
+  
 });
